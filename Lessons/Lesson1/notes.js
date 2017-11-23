176e5bd3de161102b9a508102e2dff8b210519d7
tjiasan <jason.tjia@gmail.com>
2017-11-23 14:39:48 -0500

    first lesson:

---
+++ Lessons/Lesson1/notes.js
@@ -0,0 +1,14 @@
+/*
+notes goes here!
+
+Algorithms enabled by 
+Constraints Exposed by representations
+that support models targeted at thinking
+perception, action
+
+
+Generate and Test:
+
+Generate Model --> test --> succed?
+
+*/
